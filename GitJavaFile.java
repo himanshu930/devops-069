e0d206f0aae112a19bb6c05be66f9d052828f4e4
himanshu930 <himanshuwakodikar930@gmail.com>
2024-01-14 21:18:48 +0530

    updated java file

--- GitJavaFile.java
+++ GitJavaFile.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class GitJavaFile{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World using GitHub and Jenkins");
 	}
 }
