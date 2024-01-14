f339cbe98bc133b7f8b0a9e65368fdcc70693d4c
himanshu930 <himanshuwakodikar930@gmail.com>
2024-01-14 19:05:13 +0530

    git-lab-file

---
+++ GitJavaFile.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class GitJavaFile{
+	public static void main(String[] args){
+		int a, b;
+		System.out.println("Enter two number : ");
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println("Addition : " + (a + b));
+	}
+}
