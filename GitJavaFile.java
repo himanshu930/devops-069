42e8864d71a8dfc25c650afe73a2c85f90640ddb
himanshu930 <108115168+himanshu930@users.noreply.github.com>
2024-01-14 21:11:57 +0530

    Update GitJavaFile.java

--- GitJavaFile.java
+++ GitJavaFile.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class GitJavaFile{
 	public static void main(String[] args){
-		int a, b;
-		System.out.println("Enter two number : ");
-		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		b = sc.nextInt();
-		System.out.println("Addition : " + (a + b));
+		System.out.println("Hello World");
 	}
 }
